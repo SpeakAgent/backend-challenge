ce7b294b7127a9ca6551eff13bf34c09ae56cef9
Luis J. Perez <luisjorge129@gmail.com>
2016-08-08 13:04:17 -0400

    Added default values for Student model date of birth and blank for Grade.

--- studentsite/students/models.py
+++ studentsite/students/models.py
@@ -1,12 +1,13 @@
 from __future__ import unicode_literals
+import datetime
 
 from django.db import models
 
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    grade = models.CharField(max_length=1)
+    date_of_birth = models.DateField(default=datetime.date.today)
+    grade = models.CharField(max_length=1, blank=True)
 
     def __unicode__(self):
         return u"{l}, {f}".format(
