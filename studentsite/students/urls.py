3a7332d18e16727117c17d4cc8e3694674e67512
Luis J. Perez <luisjorge129@gmail.com>
2016-08-08 13:08:56 -0400

    Added students urls.py file.

---
+++ studentsite/students/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+urlpatterns = [
+    #Class Based View
+    # url(r'^$', StudentView.as_view(), name='students'),
+
+    #Function View
+    #url(r'^$', views.StudentsView, name='students')
+]
