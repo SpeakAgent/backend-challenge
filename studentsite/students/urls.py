b19e1a28371bb368d00af78954fc22ec1af55fa7
Luis J. Perez <luisjorge129@gmail.com>
2016-08-08 19:58:00 -0400

    Added DjangoRestFramework, route for the api calls, view logic and serializers.py

--- studentsite/students/urls.py
+++ studentsite/students/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
+from django.conf.urls import include
 
 from .views import StudentView
+from .views import StudentViewSet
+
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'students', StudentViewSet)
 
 urlpatterns = [
     #Class Based View url
@@ -8,4 +16,6 @@ urlpatterns = [
 
     #Function Based View url
     # url(r'^$', StudentView, name='students')
+
+    url(r'^api/', include(router.urls)),
 ]
--- studentsite/students/views.py
+++ studentsite/students/views.py
@@ -2,16 +2,31 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from .models import Student
+from .serializers import StudentSerializer
+
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 
 # Class Based View
 class StudentView(ListView):
-    queryset = Student.objects.filter(grade='A')
+    queryset = Student.objects.filter(grade='K')
     context_object_name = 'students'
     template_name = 'student_list.html'
 
 
 # Function Based View
 # def StudentView(request):
-#     student_list = Student.objects.filter(grade='A')
+#     student_list = Student.objects.filter(grade='K')
 #     return render(request, 'student_list.html', {'students': student_list})
+
+
+# Api calls
+class StudentViewSet(ReadOnlyModelViewSet):
+    """
+    ###Retrieve student by id
+    - ####Example:
+        *  #####Student by id: [/api/students/1](/api/students/1)
+    ---
+    """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
