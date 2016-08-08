5e549992e1161aec76968ca87bb555bb4629d907
Luis J. Perez <luisjorge129@gmail.com>
2016-08-08 19:03:45 -0400

    Added Function and Class Based Views

--- studentsite/students/views.py
+++ studentsite/students/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Student
+
+
+# Class Based View
+class StudentView(ListView):
+    queryset = Student.objects.filter(grade='A')
+    context_object_name = 'students'
+    template_name = 'student_list.html'
+
+
+# Function Based View
+# def StudentView(request):
+#     student_list = Student.objects.filter(grade='A')
+#     return render(request, 'student_list.html', {'students': student_list})
