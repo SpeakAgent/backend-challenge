320f2ecbf00ada83cac1f0b54f6cb698641168df
Garrison Benson <benson.garrison@gmail.com>
2016-08-09 18:30:28 -0400

    Added grade and date of birth fields to Student model.

--- studentsite/students/models.py
+++ studentsite/students/models.py
@@ -2,9 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(blank=True, null=True)
+    grade = models.CharField(max_length=20, blank=True)
 
     def __unicode__(self):
         return u"{l}, {f}".format(
