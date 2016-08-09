432c69eb9d6581eefaa4cb48b074f38b8bde5cf2
Rachel Kelly <rkellyalso@gmail.com>
2016-08-09 14:34:28 -0700

    fields to student model

--- studentsite/students/models.py
+++ studentsite/students/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    DOB = models.CharField(max_length=10)
+    grade = models.CharField(max_length=1)
 
     def __unicode__(self):
         return u"{l}, {f}".format(
