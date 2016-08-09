15c1b504f34bbc463394791336516e758a1fa54a
Nick Watts <nick@nawatts.com>
2016-08-09 19:55:11 -0400

    Add grade and DOB to Student model

--- studentsite/students/models.py
+++ studentsite/students/models.py
@@ -2,11 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True)
+    grade = models.CharField(null=True, max_length=20)
 
     def __unicode__(self):
-        return u"{l}, {f}".format(
+        return u"{l}, {f} - Grade {g}, born {dob:%-m/%-d/%Y}".format(
             l=self.last_name,
-            f=self.first_name)
+            f=self.first_name,
+            g=self.grade,
+            dob=self.date_of_birth)
--- studentsite/students/tests.py
+++ studentsite/students/tests.py
@@ -1,3 +1,19 @@
+import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Student
+
+
+class CreateStudentTest(TestCase):
+
+    def test_create_student(self):
+        student = Student.objects.create(
+            first_name="John",
+            last_name="Doe",
+            date_of_birth=datetime.date(2010, 8, 15),
+            grade="K",
+        )
+
+        self.assertTrue(isinstance(student, Student))
+        self.assertEqual(student.__unicode__(), "Doe, John - Grade K, born 8/15/2010")
