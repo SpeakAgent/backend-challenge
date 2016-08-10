e503995ec2a4273aa9e5cfde6b4219bd2761c6cb
Garrison Benson <benson.garrison@gmail.com>
2016-08-09 22:25:48 -0400

    Made student list admin page more readable.

--- studentsite/students/admin.py
+++ studentsite/students/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from students.models import Student
 
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'grade', 'date_of_birth')
 
 admin.site.register(Student, StudentAdmin)
