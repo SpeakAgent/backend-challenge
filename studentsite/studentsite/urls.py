8839321d249066b9f004aed81837376715aa382f
Nick Watts <nick@nawatts.com>
2016-08-09 22:36:04 -0400

    Redirect site root to students list.

--- studentsite/studentsite/urls.py
+++ studentsite/studentsite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    url(r"^$", RedirectView.as_view(url="/students")),
     url(r'^students/', include('students.urls')),
     url(r'^admin/', admin.site.urls),
 ]
