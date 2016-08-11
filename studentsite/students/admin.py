0f5af3f2a8efca28943f1418946bbd77e199be67
Nick Watts <nick@nawatts.com>
2016-08-11 18:50:37 -0400

    Add Schools and Teachers to admin site

--- studentsite/students/admin.py
+++ studentsite/students/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
-from students.models import Student
+from students.models import School, Student, Teacher
+
+
+class SchoolAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(School, SchoolAdmin)
+
 
 class StudentAdmin(admin.ModelAdmin):
     pass
 
+
 admin.site.register(Student, StudentAdmin)
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Teacher, TeacherAdmin)
