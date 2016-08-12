01eec1b3af02d6040062ed39ae3981e2100e19bc
Rachel Kelly <rkellyalso@gmail.com>
2016-08-11 17:34:48 -0700

    formatting student outputs

--- studentsite/students/models.py
+++ studentsite/students/models.py
@@ -6,9 +6,11 @@ class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     DOB = models.CharField(max_length=10)
-    grade = models.CharField(max_length=1)
+    grade = models.CharField(max_length=2)
 
     def __unicode__(self):
-        return u"{l}, {f}".format(
+        return u"{l}, {f} - Grade {g}, born {d}\n".format(
             l=self.last_name,
-            f=self.first_name)
+            f=self.first_name,
+            d=self.DOB,
+            g=self.grade)
