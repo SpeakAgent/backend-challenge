76a08cc655ab8bdc1c4e04545cead556b80ff068
Rachel Kelly <rkellyalso@gmail.com>
2016-08-13 19:29:35 -0700

    added new models

--- studentsite/students/models.py
+++ studentsite/students/models.py
@@ -14,3 +14,29 @@ class Student(models.Model):
             f=self.first_name,
             d=self.DOB,
             g=self.grade)
+
+class School(models.Model):
+    title = models.CharField(max_length=50)
+    #condense 4 following to one 4-line TextField
+    address1 = models.CharField(max_length=40)
+    address2 = models.CharField(max_length=40)
+    address3 = models.CharField(max_length=40)
+    address4 = models.CharField(max_length=40)
+    
+    def __unicode__(self):
+        return u"{t}\n{a1}\n{a2}\n{a3}\n{a4}".format(
+            t=self.title,
+            a1=self.address1,
+            a2=self.address2,
+            a3=self.address3,
+            a4=self.address4)
+
+class Teacher(models.Model):
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return u"{l}, {f}".format(
+            l=self.last_name,
+            f=self.first_name)
+
