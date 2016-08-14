e433ccfc148f481a5d1be2d32cbe729ac6ed33d5
Rachel Kelly <rkellyalso@gmail.com>
2016-08-13 22:37:05 -0700

    last drf commit for now.  no access to drf yet.

--- studentsite/students/serializers.py
+++ studentsite/students/serializers.py
@@ -1,4 +1,4 @@
-from model import Student
+from students.models import Student
 
 from rest_framework import serializers
 
@@ -6,3 +6,9 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ('last_name', 'first_name', 'DOB', 'grade',)
+
+class StudentsMasterList(serializers.ModelSerializer):
+    class Meta:
+        StudentSerializer.objects.all()
+        #all_students = Student.objects.all()
+        #return all_students
