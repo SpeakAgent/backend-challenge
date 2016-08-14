70c4a852252f9baca995350de8861e25f75ccd9b
Rachel Kelly <rkellyalso@gmail.com>
2016-08-13 23:11:52 -0700

    As functional on local heroku web as pre-heroking

--- studentsite/studentsite/settings.py
+++ studentsite/studentsite/settings.py
@@ -14,7 +14,7 @@ import os
 
 import dj_database_url #heroku import & below 2lines
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
